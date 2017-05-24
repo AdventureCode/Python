ab5580d4267f1026ae82c550ae082482a09ed6f2
Fernando <fernandomaciel@ufrn.edu.br>
2017-05-24 20:56:19 -0300

    Add files via upload

---
+++ python/exercicios/ES/respostasES/11ES.py
@@ -0,0 +1,11 @@
+a = int(input("Informe um numero inteiro: "))
+b = int(input("Informe um numero inteiro: "))
+c = float(input("Informe um numero real: "))
+
+n = (a * 2) * (b / 2)
+n1 = (a * 3) + (c)
+n2 = (c**3)
+
+print(n)
+print(n1)
+print(n2)
